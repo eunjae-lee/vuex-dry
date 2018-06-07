e4dd32feedd91cf2b109cf6998d5d3c54c3e6370
Paul Lee <paul@valuepotion.com>
2018-06-07 22:42:50 +0800

    Add first test case

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
