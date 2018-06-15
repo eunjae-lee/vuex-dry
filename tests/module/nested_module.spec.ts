e63c140ac0f151714b2a46a9e72e30929c21db05
Paul Lee <paul@valuepotion.com>
2018-06-15 11:09:59 +0800

    Add test case

--- tests/module/nested_module.spec.ts
+++ tests/module/nested_module.spec.ts
@@ -34,15 +34,38 @@ const sampleStore = () => {
 };
 
 describe("Nested module", () => {
-  it("works fine", () => {
+  it("works", () => {
     const store = sampleStore();
     store.commit("company/owner/name$assign", "Paul");
     expect(store.getters["company/owner/name"]).toEqual("Paul");
   });
 
-  it("works fine with $set", () => {
+  it("works with $set", () => {
     const store = sampleStore();
     store.commit("company/owner/profile$set", { key: "bio", value: "world" });
     expect(store.getters["company/owner/profile$get"]("bio")).toEqual("world");
+  });
+
+  it("works with $reset", () => {
+    const user = Module.build({
+      config: {
+        nonStrictObject: ["profile"]
+      },
+      state() {
+        return {
+          profile: {}
+        };
+      }
+    });
+    const store = new Vuex.Store({
+      modules: {
+        user
+      }
+    });
+
+    store.commit("user/profile$set", { key: "bio", value: "hello" });
+    expect(store.getters["user/profile$get"]("bio")).toEqual("hello");
+    store.commit("user/profile$reset");
+    expect(store.getters["user/profile$get"]("bio")).toEqual(undefined);
   });
 });
